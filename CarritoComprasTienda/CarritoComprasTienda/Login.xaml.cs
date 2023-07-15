d39d04781e38ff6292ee2d6ebaad55f3d676d5a7
Esteban <estebin-sa@hotmail.com>
2023-07-15 12:18:11 -0500

    login con texto

--- CarritoComprasTienda/CarritoComprasTienda/Login.xaml.cs
+++ CarritoComprasTienda/CarritoComprasTienda/Login.xaml.cs
@@ -16,5 +16,25 @@ namespace CarritoComprasTienda
         {
             InitializeComponent();
         }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "uisrael2023";
+            string contraseña = "esteban2023";
+            if (txtUsuario.Text == usuario && txtContraseña.Text == contraseña)
+            {
+                //Navigation.PushAsync(new Compras(usuario));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario/Contraseña incorrectos", "Cerrar");
+            }
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            txtUsuario.Text = "";
+            txtContraseña.Text = "";
+        }
     }
 }
